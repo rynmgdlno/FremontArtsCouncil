8ba3606062b9be75b3359d9f947e9694a0dc13cb
rynmgdlno <rynmgdlno@gmail.com>
2021-02-08 12:42:03 -0800

    sponsor links

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -44,11 +44,11 @@ const Home = () => {
       <div className='sponsors'>
         <h3>Our Sponsors</h3>
         <div className='sponsors-logos'>
-          <a href="https://www.tpnevents.com"><img className='tpn-logo-image' alt='sponsor logo' src={tpn} /></a>
-          <a href="https://nwartstream.org"><img className='nash-logo-image' alt='sponsor logo' src={nash} /></a>
-          <a href="https://www.4culture.org"><img className='four-logo-image' alt='sponsor logo' src={fourLogo} /></a>
-          <a href="https://www.cheerseattle.org"><img className='cheer-logo-image' alt='sponsor logo' src={cheer} /></a>
-          <a href="https://www.mipops.org"><img className='mipops-logo-image' alt='sponsor logo' src={mipops} /></a>
+          <a href="https://www.tpnevents.com"><img className='tpn-logo-image sponsor-link' alt='sponsor logo' src={tpn} /></a>
+          <a href="https://nwartstream.org"><img className='nash-logo-image sponsor-link' alt='sponsor logo' src={nash} /></a>
+          <a href="https://www.4culture.org"><img className='four-logo-image sponsor-link' alt='sponsor logo' src={fourLogo} /></a>
+          <a href="https://www.cheerseattle.org"><img className='cheer-logo-image sponsor-link' alt='sponsor logo' src={cheer} /></a>
+          <a href="https://www.mipops.org"><img className='mipops-logo-image sponsor-link' alt='sponsor logo' src={mipops} /></a>
         </div>
       </div>
     </div>
