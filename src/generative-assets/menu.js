4480ed65e89ef3187a0eaf7acfb258dffd1218dd
rynmgdlno <rynmgdlno@gmail.com>
2021-02-09 15:50:46 -0800

    menu fix

--- src/generative-assets/menu.js
+++ src/generative-assets/menu.js
@@ -1,175 +1,175 @@
 const menuTree = [
   {
-    id: 0,
+    id: 1,
     name: 'About Us',
     subMenu: [
       {
-        id: 0,
+        id: 1,
         name: 'Mission',
         link: '/mission'
       },
       {
-        id: 1,
+        id: 2,
         name: 'Our Story',
         link: '/story'
       },
       {
-        id: 2,
+        id: 3,
         name: 'Commitment',
         link: '/commitment'
       },
       {
-        id: 3,
+        id: 4,
         name: 'Fremont Troll',
         link: '/troll'
       },
       {
-        id: 4,
+        id: 5,
         name: 'Board Members',
         link: '/board'
       },
       {
-        id: 5,
+        id: 6,
         name: 'Contact us',
         link: '/contact'
       },
       {
-        id: 6,
+        id: 7,
         name: 'Sponsors',
         link: '/sponsors'
       }
     ]
   },
   {
-    id: 1,
+    id: 2,
     name: 'News',
     subMenu: [
       {
-        id: 0,
+        id: 1,
         name: 'Latest News',
         link: '/latest'
       },
       {
-        id: 1,
+        id: 2,
         name: 'Press',
         link: '/press'
       }
     ]
   },
   {
-    id: 2,
+    id: 3,
     name: 'Events',
     subMenu: [
       {
-        id: 0,
+        id: 1,
         name: 'Upcoming Events',
         link: '/upcoming-events'
       },
       {
-        id: 1,
+        id: 2,
         name: 'Fremont Solstice Parade',
         link: '/parade'
       },
       {
-        id: 2,
+        id: 3,
         name: 'Solstice Celebration',
         link: '/solstice'
       },
       {
-        id: 3,
+        id: 4,
         name: 'Luminata',
         link: '/luminata'
       },
       {
-        id: 4,
+        id: 5,
         name: 'Trolloween',
         link: '/trolloween'
       },
       {
-        id: 5,
+        id: 6,
         name: 'Feast of the Winter Solstice',
         link: '/winter-feast'
       },
       {
-        id: 6,
+        id: 7,
         name: 'May Day',
         link: '/may-day'
       }
     ]
   },
   {
-    id: 3,
+    id: 4,
     name: 'Fremont Solstice Parade',
     subMenu: [
       {
-        id: 0,
+        id: 1,
         name: 'About the Parade',
         link: '/parade'
       },
       {
-        id: 1,
+        id: 2,
         name: 'Participate',
         link: '/participate'
       },
       {
-        id: 2,
+        id: 3,
         name: 'Gallery',
         link: '/gallery'
       },
       {
-        id: 3,
+        id: 4,
         name: 'Sponsors',
         link: '/sponsors'
       },
       {
-        id: 4,
+        id: 5,
         name: 'Solstice Celebration',
         link: '/solstice'
       }
     ]
   },
   {
-    id: 4,
+    id: 5,
     name: 'Workshops',
     subMenu: [
       {
-        id: 0,
+        id: 1,
         name: 'Upcoming Workshops',
         link: '/workshops'
       },
       {
-        id: 1,
+        id: 2,
         name: 'Teach a Workshop',
         link: '/teach-a-workshop'
       }
     ]
   },
   {
-    id: 5,
+    id: 6,
     name: 'Get Involved',
     subMenu: [
       {
-        id: 0,
+        id: 1,
         name: 'Artists',
         link: '/artists'
       },
       {
-        id: 1,
+        id: 2,
         name: 'Membership',
         link: '/membership'
       },
       {
-        id: 2,
+        id: 3,
         name: 'Donate Now',
         link: '/donate'
       },
       {
-        id: 3,
+        id: 4,
         name: 'Sponsor Us',
         link: '/sponsor-us'
       },
       {
-        id: 4,
+        id: 5,
         name: 'Community Meetings',
         link: '/community-meetings'
       }
