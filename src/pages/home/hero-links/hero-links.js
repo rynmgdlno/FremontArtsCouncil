399e60c77cf4c9ed254656c12066bbb015b4619e
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-02-11 14:52:33 -0800

    home page styles

--- src/pages/home/hero-links/hero-links.js
+++ src/pages/home/hero-links/hero-links.js
@@ -6,24 +6,22 @@ import './hero-links.scss'
 
 const HeroLinks = () => {
   return (
-    <div>
-      <div className='hero-links'>
-        <Link className='card-container news-area' to='/latest'>
-          <div className='news card'>
-            <h2 className='card-title'>News</h2>
-          </div>
-        </Link>
-        <Link className='card-container workshops-area' to='/workshops'>
-          <div className='workshops card'>
-            <h2 className='card-title'>Workshops</h2>
-          </div>
-        </Link>
-        <Link className='card-container events-area' to='/upcoming-events'>
-          <div className='events card'>
-            <h2 className='card-title'>Events</h2>
-          </div>
-        </Link>
-      </div>
+    <div className='hero-links'>
+      <Link className='card-container news-area' to='/latest'>
+        <div className='news card'>
+          <h2 className='card-title'>News</h2>
+        </div>
+      </Link>
+      <Link className='card-container workshops-area' to='/workshops'>
+        <div className='workshops card'>
+          <h2 className='card-title'>Workshops</h2>
+        </div>
+      </Link>
+      <Link className='card-container events-area' to='/upcoming-events'>
+        <div className='events card'>
+          <h2 className='card-title'>Events</h2>
+        </div>
+      </Link>
     </div>
   )
 }
--- src/pages/home/leader/leader.js
+++ src/pages/home/leader/leader.js
@@ -4,14 +4,12 @@ import './leader.scss'
 
 const Leader = () => {
   return (
-    <div>
-      <div className='leader'>
-        <div className='leader-text'>
-          <h1>Where Everyone is an Artist</h1>
-          <h3>The Fremont Arts Council engages community to cultivate the spirit of celebration where everyone is an artist.</h3>
-        </div>
-        <div className='leader-image'></div>
+    <div className='leader'>
+      <div className='leader-text'>
+        <h1>Where Everyone is an Artist</h1>
+        <h3>The Fremont Arts Council engages community to cultivate the spirit of celebration where everyone is an artist.</h3>
       </div>
+      <div className='leader-image'></div>
     </div>
   )
 }
