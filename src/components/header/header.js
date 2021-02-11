bbae50e24b924c96dcca8df7ea162d22516e6fcd
rynmgdlno <rynmgdlno@gmail.com>
2021-02-10 22:19:36 -0800

    mobile styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
+import { useMediaPredicate } from 'react-media-hook'
+import Hamburger from 'hamburger-react'
 
 // import Logo from "../logo/logo";
 import Logo from '../../component-svgs/logo'
@@ -9,14 +11,21 @@ import CustomButton from "../custom-button/custom-button";
 import './header.scss'
 
 const Header = () => {
+  const [isOpen, setOpen] = useState(false)
+  const mobileQuery = useMediaPredicate('(max-width: 767px)')
+
   return (
     <div className='header'>
       <div className='top-bar'>
         <Link to='/'><Logo /></Link>
+        {
+          mobileQuery ? <Hamburger toggled={isOpen} toggle={setOpen} /> :
         <div className='header-button-container'>
           <CustomButton className='custom-button medium-emphasis-button donate-button'>Donate</CustomButton>
           <CustomButton className='custom-button medium-emphasis-button volunteer-button'>Volunteer</CustomButton>
         </div>
+        }
+        
       </div>
       <Menu />
       <div className='border'></div>
