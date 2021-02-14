2f3aaafb6a6acfaa6dbd951d8529badc29882d6e
rynmgdlno <rynmgdlno@gmail.com>
2021-02-13 18:24:31 -0800

    contentful page header implementation

--- src/components/page-header/page-header.js
+++ src/components/page-header/page-header.js
@@ -9,13 +9,15 @@ const PageHeader = ({headerID}) => {
   const [header, isloading] = useHeader(headerID)
 
   if (isloading) return <p>Loading...</p>
+  console.log(header)
   return (
     <div className='page-header'>
       <div className='page-header-text'>
         <h1 className='page-header-title'>{header[0]}</h1>
         <h3 className='page-header-subtitle'>{header[1]}</h3>
+        <h4>{header[3]}</h4>
       </div>
-      <img className='page-header-image' src={header[2]} alt=''/>
+      <img className='page-header-image' src={header[2]} alt='test stuff'/>
     </div>
   )
 } 
