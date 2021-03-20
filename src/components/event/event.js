d4d66cd7c4b13cfdaaf620a1bf9ff2dbe5913e28
rynmgdlno <rynmgdlno@gmail.com>
2021-03-19 17:15:01 -0700

    contentful event setup

--- src/components/event/event.js
+++ src/components/event/event.js
@@ -18,6 +18,29 @@ const Event = ({eventID}) => {
         </div>
         <img className='event-header-image' src={event[6]} alt=''/>
       </div>
+      <div className='event-info'>
+        <span>Time</span>
+        <h2>{event[2]}</h2>
+        <h2>1 PM  - 4 PM</h2>
+        <span>Location</span>
+        <h4>{event[7]}</h4>
+        <h4>{event[1]}</h4>
+      </div>
+      <div className='event-description'>
+        <h2>About {event[11]}</h2>
+        <p>{event[0]}</p>
+      </div>
+      <div className='event-location'>
+        <div className='location-description-container'>
+          <h2 className='location-title'>{event[10]}</h2>
+          <p>{event[9]}</p>
+        </div>
+        <div className='map-container'>
+          <img className='location-map' src={event[8]} alt=''/>
+        </div>
+      </div>
+      <div className='event-involved'></div>
+      <div className='event-previous'></div>
     </div>
   )
 }
