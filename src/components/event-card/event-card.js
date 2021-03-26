14909e819127a685683274e0a614df80fe425fff
rynmgdlno <rynmgdlno@gmail.com>
2021-03-26 14:24:44 -0700

    upcoming events page

--- src/components/event-card/event-card.js
+++ src/components/event-card/event-card.js
@@ -1,18 +1,38 @@
 import React from 'react'
+import { useMediaPredicate } from 'react-media-hook'
+
+import CustomButton from '../custom-button/custom-button'
 
 import './event-card.scss'
 
-const EventCard = ({className, image, title, text, date, id}) => {
+const EventCard = ({ className, image, title, text, date, id }) => {
+  const isMobile = useMediaPredicate('(max-width: 769px)')
 
   return (
-    <div className={className}>
-      <img src={`https:${image}`} alt=''/>
-      <h2>{title}</h2>
-      <span>{date}</span>
-      <p>{text}</p>
-      <span>{id}</span>
-    </div>
+    <>
+      {
+        !isMobile ?
+          <div className={className}>
+            <img className='event-card-image' src={`https:${image}`} alt='' />
+            <div className='event-card-info-container'>
+              <span>{date}</span>
+              <h2>{title}</h2>
+              <p>{text}</p>
+              <CustomButton>{id}</CustomButton>
+            </div>
+          </div>
+          :
+          <div>
+            <span>{date}</span>
+            <h2>{title}</h2>
+            <img className='event-card-image' src={`https:${image}`} alt='' />
+            <p>{text}</p>
+            <CustomButton>{id}</CustomButton>
+          </div>
+      }
+    </>
   )
 }
 
 export default EventCard
+
