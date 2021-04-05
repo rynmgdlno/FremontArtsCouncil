f977a86e1da15255a5cebcc78081ffe163b47aeb
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-04-05 14:57:38 -0700

    our workshops

---
+++ src/component-svgs/workshops/fan.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import './fan.scss'
+
+const Fan = () => {
+  return (
+    <div className='fan'>
+      <svg className='fan1' width="259" height="261" viewBox="0 0 259 261" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M303.593 260.403L0.106697 73.1806L44.7421 0.27342L54.5027 10.0685L303.593 260.403Z" fill="#AD5FBE" />
+      </svg>
+      <svg className='fan2' width="203" height="280" viewBox="0 0 203 280" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M60.4337 0.586441C60.4337 0.586441 54.5544 3.40932 47.2608 10.5517C39.9672 17.694 0.032809 46.6832 0.032809 46.6832L242.287 279.376L60.4337 0.586441Z" fill="#EC9028" />
+      </svg>
+      <svg className='fan3' width="134" height="292" viewBox="0 0 134 292" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.119751 31.7936L55.0574 0.589643L176.092 291.532L0.119751 31.7936Z" fill="#AD5FBE" />
+      </svg>
+      <svg className='fan4' width="76" height="289" viewBox="0 0 76 289" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0.136988 14.8301L59.188 0.310039L120.044 288.953L0.136988 14.8301Z" fill="#EC9028" />
+      </svg>
+
+    </div>
+  )
+}
+
+export default Fan
--- src/pages/workshops/workshops.js
+++ src/pages/workshops/workshops.js
@@ -1,13 +1,55 @@
 import React from 'react'
 
 import PageHeader from '../../components/page-header/page-header'
+import CustomButton from '../../components/custom-button/custom-button'
+
+import lumLanWor from '../../images/workshops/Rectangle 291.png'
+import larLumWor from '../../images/workshops/Rectangle 299.png'
+import teach from '../../images/workshops/Rectangle 300.png'
+
+import TopBlob from '../../component-svgs/workshops/top-blob'
+import LeftBlobs from '../../component-svgs/workshops/left-blobs'
+import Fan from '../../component-svgs/workshops/fan'
+import BrBlobs from '../../component-svgs/workshops/br-blobs'
+import BlBlobs from '../../component-svgs/workshops/bl-blobs'
 
 import './workshops.scss'
 
 const Workshops = () => {
-  return ( 
-    <div>
-      <PageHeader headerID={'5ilpvgxauhJNZjqbBGSrHn'}/>
+  return (
+    <div className='our-workshops'>
+      <PageHeader headerID={'5ilpvgxauhJNZjqbBGSrHn'} />
+      <TopBlob />
+      <LeftBlobs />
+      <Fan />
+      <BlBlobs />
+      <BrBlobs />
+      <div className='workshop-page-container'>
+        <div className='workshop-entry'>
+          <img className='workshop-image' src={lumLanWor} alt='' />
+          <div className='workshop-info-container'>
+            <h3 className='workshop-title'>Luminary Lantern Workshops</h3>
+            <p className='workshop-info'>Thursdays, Fridays, Saturdays, and Sundays. Join us in making lanterns for Luminata!</p>
+            <CustomButton className='custom-button high-emphasis-button blue-button'>Register</CustomButton>
+          </div>
+        </div>
+        <div className='workshop-entry'>
+          <img className='workshop-image' src={larLumWor} alt='' />
+          <div className='workshop-info-container'>
+            <h3 className='workshop-title'>Large Luminary Workshop</h3>
+            <p className='workshop-info'>Weekly Wednesday workshop series led by Sarah Lovett. Meets for 5 weeks starting 8/21, 7-10 PM</p>
+            <CustomButton className='custom-button high-emphasis-button blue-button'>Register</CustomButton>
+          </div>
+        </div>
+        <div className='workshop-entry'>
+          <img className='workshop-image' src={teach} alt='' />
+          <div className='workshop-info-container'>
+            <h3 className='workshop-title'>Teach a Workshop</h3>
+            <p className='workshop-info'>Volunteers are always needed to help teach our art workshops and other FAC events. </p>
+            <CustomButton className='custom-button high-emphasis-button blue-button'>Sign Up</CustomButton>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
