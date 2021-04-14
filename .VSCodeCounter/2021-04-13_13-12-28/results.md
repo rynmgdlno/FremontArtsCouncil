2ad678b61624897ef425939d4e30f0a8ca676beb
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-04-14 10:10:12 -0700

    membership page

---
+++ .VSCodeCounter/2021-04-13_13-12-28/results.md
@@ -0,0 +1,112 @@
+# Summary
+
+Date : 2021-04-13 13:12:28
+
+Directory /Volumes/Workspace/Web Dev/FremontArtsCouncil
+
+Total : 193 files,  39131 codes, 151 comments, 906 blanks, all 40188 lines
+
+[details](details.md)
+
+## Languages
+| language | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| JSON | 3 | 34,448 | 0 | 3 | 34,451 |
+| JavaScript | 108 | 2,712 | 60 | 471 | 3,243 |
+| SCSS | 71 | 1,851 | 41 | 389 | 2,281 |
+| XML | 8 | 63 | 0 | 7 | 70 |
+| Markdown | 1 | 38 | 0 | 33 | 71 |
+| HTML | 1 | 19 | 27 | 3 | 49 |
+| CSS | 1 | 0 | 23 | 0 | 23 |
+
+## Directories
+| path | files | code | comment | blank | total |
+| :--- | ---: | ---: | ---: | ---: | ---: |
+| . | 193 | 39,131 | 151 | 906 | 40,188 |
+| assets | 1 | 44 | 0 | 1 | 45 |
+| assets/images | 1 | 44 | 0 | 1 | 45 |
+| public | 2 | 44 | 27 | 4 | 75 |
+| src | 187 | 4,582 | 124 | 866 | 5,572 |
+| src/component-svgs | 24 | 464 | 2 | 69 | 535 |
+| src/component-svgs/green-numbers | 3 | 30 | 0 | 9 | 39 |
+| src/component-svgs/workshop-info | 6 | 129 | 1 | 20 | 150 |
+| src/component-svgs/workshops | 10 | 203 | 0 | 28 | 231 |
+| src/components | 59 | 2,109 | 67 | 376 | 2,552 |
+| src/components/arrow-button | 2 | 18 | 0 | 4 | 22 |
+| src/components/container | 2 | 51 | 2 | 9 | 62 |
+| src/components/custom-button | 2 | 107 | 1 | 22 | 130 |
+| src/components/event | 8 | 508 | 3 | 79 | 590 |
+| src/components/event-calendar | 6 | 188 | 3 | 32 | 223 |
+| src/components/event-calendar/month | 4 | 50 | 2 | 13 | 65 |
+| src/components/event-calendar/month/month-event-card | 2 | 41 | 2 | 8 | 51 |
+| src/components/event-card | 3 | 124 | 0 | 16 | 140 |
+| src/components/event/graphic-components | 4 | 36 | 0 | 11 | 47 |
+| src/components/event/involved-card | 2 | 50 | 2 | 10 | 62 |
+| src/components/footer | 9 | 237 | 0 | 47 | 284 |
+| src/components/footer/footer-form | 2 | 58 | 0 | 9 | 67 |
+| src/components/footer/footer-images | 5 | 60 | 0 | 16 | 76 |
+| src/components/form-input | 2 | 61 | 1 | 7 | 69 |
+| src/components/gallery-modal | 2 | 27 | 2 | 4 | 33 |
+| src/components/header | 2 | 114 | 3 | 19 | 136 |
+| src/components/info-page | 2 | 117 | 1 | 29 | 147 |
+| src/components/logo | 2 | 10 | 0 | 4 | 14 |
+| src/components/menu | 2 | 90 | 43 | 19 | 152 |
+| src/components/menu-item | 2 | 155 | 5 | 25 | 185 |
+| src/components/mobile-menu | 2 | 19 | 1 | 7 | 27 |
+| src/components/news-card | 2 | 50 | 0 | 8 | 58 |
+| src/components/page-header | 2 | 53 | 2 | 11 | 66 |
+| src/components/press-card | 2 | 53 | 0 | 11 | 64 |
+| src/components/view-button | 3 | 51 | 0 | 9 | 60 |
+| src/components/workshop-info | 2 | 76 | 0 | 14 | 90 |
+| src/contentful-hooks | 5 | 70 | 0 | 29 | 99 |
+| src/generative-assets | 1 | 209 | 0 | 1 | 210 |
+| src/global | 1 | 13 | 2 | 3 | 18 |
+| src/hooks | 1 | 33 | 0 | 10 | 43 |
+| src/images | 6 | 18 | 0 | 6 | 24 |
+| src/images/footer | 3 | 9 | 0 | 3 | 12 |
+| src/images/homepage | 3 | 9 | 0 | 3 | 12 |
+| src/images/homepage/leader | 3 | 9 | 0 | 3 | 12 |
+| src/pages | 80 | 1,454 | 15 | 332 | 1,801 |
+| src/pages/about | 2 | 9 | 0 | 5 | 14 |
+| src/pages/artists | 2 | 10 | 0 | 4 | 14 |
+| src/pages/board | 2 | 9 | 0 | 5 | 14 |
+| src/pages/commitment | 2 | 9 | 0 | 5 | 14 |
+| src/pages/contact | 2 | 9 | 0 | 5 | 14 |
+| src/pages/donate | 2 | 74 | 0 | 7 | 81 |
+| src/pages/events | 12 | 113 | 0 | 35 | 148 |
+| src/pages/events/luminata | 2 | 11 | 0 | 5 | 16 |
+| src/pages/events/mayday | 2 | 11 | 0 | 5 | 16 |
+| src/pages/events/solstice | 2 | 11 | 0 | 5 | 16 |
+| src/pages/events/trolloween | 2 | 11 | 0 | 5 | 16 |
+| src/pages/events/winter-feast | 2 | 11 | 0 | 5 | 16 |
+| src/pages/home | 21 | 436 | 13 | 92 | 541 |
+| src/pages/home/get-inspired | 2 | 17 | 0 | 3 | 20 |
+| src/pages/home/get-involved | 4 | 92 | 0 | 17 | 109 |
+| src/pages/home/get-involved/card | 2 | 19 | 0 | 5 | 24 |
+| src/pages/home/hero-links | 2 | 94 | 2 | 16 | 112 |
+| src/pages/home/leader | 7 | 113 | 0 | 24 | 137 |
+| src/pages/home/leader/leader-image | 5 | 73 | 0 | 16 | 89 |
+| src/pages/home/our-sponsors | 2 | 75 | 6 | 19 | 100 |
+| src/pages/home/upcoming-events-workshops | 2 | 19 | 0 | 5 | 24 |
+| src/pages/latest | 2 | 95 | 2 | 23 | 120 |
+| src/pages/meetings | 2 | 10 | 0 | 4 | 14 |
+| src/pages/membership | 2 | 209 | 0 | 31 | 240 |
+| src/pages/mission | 2 | 9 | 0 | 5 | 14 |
+| src/pages/parade | 8 | 43 | 0 | 21 | 64 |
+| src/pages/parade/gallery | 2 | 10 | 0 | 4 | 14 |
+| src/pages/parade/participate | 2 | 11 | 0 | 5 | 16 |
+| src/pages/parade/register | 2 | 11 | 0 | 5 | 16 |
+| src/pages/press | 2 | 41 | 0 | 10 | 51 |
+| src/pages/secondary-event | 2 | 13 | 0 | 8 | 21 |
+| src/pages/sponsor-us | 2 | 10 | 0 | 4 | 14 |
+| src/pages/sponsors | 2 | 47 | 0 | 8 | 55 |
+| src/pages/story | 2 | 9 | 0 | 5 | 14 |
+| src/pages/troll | 2 | 9 | 0 | 5 | 14 |
+| src/pages/volunteer | 2 | 158 | 0 | 24 | 182 |
+| src/pages/workshops | 5 | 132 | 0 | 26 | 158 |
+| src/pages/workshops/large-luminary-workshop | 1 | 12 | 0 | 3 | 15 |
+| src/pages/workshops/luminary-lantern-workshops | 1 | 12 | 0 | 4 | 16 |
+| src/pages/workshops/teach-a-workshop | 1 | 12 | 0 | 4 | 16 |
+| src/templates | 1 | 11 | 0 | 4 | 15 |
+
+[details](details.md)
--- src/pages/membership/membership.js
+++ src/pages/membership/membership.js
@@ -1,13 +1,132 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PageHeader from '../../components/page-header/page-header'
 
 import './membership.scss'
 
 const Membership = () => {
-  return ( 
+  const [level, setLevel] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [displayForm, setForm] = useState(false)
+  const [confirmation, setConfirmation] = useState(false)
+
+  console.log(level, amount)
+  console.log(displayForm)
+
+  return (
     <div>
-      <PageHeader headerID={'1qvzCME4OZARJ8yz3IfN6u'}/>
+      <PageHeader headerID={'1qvzCME4OZARJ8yz3IfN6u'} />
+      {
+        !displayForm ?
+          <div className='membership'>
+            <div className='membership-text'>
+              <p className='mem-text-line'><span className='mem-text-bold'>Connect</span> with our eclectic community.</p>
+              <p className='mem-text-line'><span className='mem-text-bold'>Stay in the loop</span> about our upcoming events and workshops.</p>
+              <p className='mem-text-line'><span className='mem-text-bold'>Sustain</span> our legendary events.</p>
+              <p className='mem-text-line'><span className='mem-text-bold'>Support us</span> in providing art supplies, keeping the lights on, and residents entertained.</p>
+            </div>
+            <h2 className='mem-header'>Our Supporters</h2>
+            <div className='membership-card-container'>
+              <div className='patron-headers'>
+                <p className='mem-text-line'><span className='mem-text-bold'>Patrons.</span> For our most generous members</p>
+                <p className='mem-text-line'><span className='mem-text-bold'>Living Lightly.</span> Support us however you can.</p>
+              </div>
+              <div className='individual-cards'>
+                <div
+                  onClick={() => {
+                    setAmount(50)
+                    setLevel('Individual')
+                    setForm(true)
+                  }}
+                  className='mem-card'>
+                  <h3>Individual</h3>
+                  <h1 className='amount'>$50</h1>
+                  <div className='liv-light'>
+                    <p>Living Lightly - $25</p>
+                    <div className='mem-card-dot' />
+                  </div>
+                  <p className='renew'>Join Or Renew</p>
+                </div>
+                <div
+                  onClick={() => {
+                    setAmount(100)
+                    setLevel('Family')
+                    setForm(true)
+                  }}
+                  className='mem-card'>
+                  <h3>Family</h3>
+                  <h1 className='amount'>$100</h1>
+                  <div className='liv-light'>
+                    <p>Living Lightly - $60</p>
+                    <div className='mem-card-dot'></div>
+                  </div>
+                  <p className='renew'>Join Or Renew</p>
+                </div>
+                <div
+                  onClick={() => {
+                    setAmount(500)
+                    setLevel('Patrons')
+                    setForm(true)
+                  }}
+                  className='mem-card'>
+                  <h3>Patrons</h3>
+                  <h1 className='amount'>$500</h1>
+                  <div className='liv-light'>
+                    <p>Living Lightly - $ 250</p>
+                    <div className='mem-card-dot'></div>
+                  </div>
+                  <p className='renew'>Join Or Renew</p>
+                </div>
+              </div>
+              <h3 className='mem-header'>Business Patronage</h3>
+              <div className='business-cards'>
+                <div
+                  onClick={() => {
+                    setAmount(500)
+                    setLevel('Business')
+                    setForm(true)
+                  }}
+                  className='biz-mem-card'>
+                  <h1 className='amount'>$500</h1>
+                  <p className='renew'>Join Or Renew</p>
+                </div>
+                <div
+                  onClick={() => {
+                    setAmount(1000)
+                    setLevel('Business')
+                    setForm(true)
+                  }}
+                  className='biz-mem-card'>
+                  <h1 className='amount'>$1000</h1>
+                  <p className='renew'>Join Or Renew</p>
+                </div>
+                <div
+                  onClick={() => {
+                    setAmount(2500)
+                    setLevel('Business')
+                    setForm(true)
+                  }}
+                  className='biz-mem-card'>
+                  <h1 className='amount'>$2500</h1>
+                  <p className='renew'>Join Or Renew</p>
+                </div>
+                <div
+                  onClick={() => {
+                    setAmount(5000)
+                    setLevel('Business')
+                    setForm(true)
+                  }}
+                  className='biz-mem-card'>
+                  <h1 className='amount'>$5000</h1>
+                  <p className='renew'>Join Or Renew</p>
+                </div>
+              </div>
+            </div>
+            <h3 className='money'>Where your money is going</h3>
+          </div>
+          :
+          <div className='membership-form'>{level}{amount}</div>
+      }
     </div>
   )
 }
