48cd49f0e93a14e76ec8317c48f4a2a945b31585
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-04-21 18:15:11 -0700

    general page template and arists, sponsor us, and COW pages

---
+++ src/pages/photography/photography.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import GeneralPage from '../../components/general-page/general-page'
+
+import './photography.scss'
+
+const Photography = () => {
+  return (
+    <GeneralPage id='7bcCn8d0ufjul5Gy71CgYa' />
+  )
+}
+
+export default Photography
