756ca23ab94c0f5a5e0ef3d79627a45bb488762b
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-05-26 20:08:49 -0700

    initial stripe elements testing

--- src/stripe/stripe-form.js
+++ src/stripe/stripe-form.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Elements, CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
+import React from 'react'
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
 
 import CustomButton from '../components/custom-button/custom-button'
 
@@ -9,41 +9,58 @@ const StripeForm = ({ isDonation, product, frequency, amount }) => {
   const stripe = useStripe()
   const elements = useElements()
 
+  const testParams = {
+    crossDomain: true,
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      'name': 'ryan',
+      'number': 12345
+    })
+  }
+
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
+    const { paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
       card: elements.getElement(CardElement)
     })
 
-    if (!error) {
-      const { id } = paymentMethod
+    const { id } = paymentMethod
 
-      try {
-        const fetchParams = {
-          crossDomain: true,
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            'id': id,
-            'amount': 12345
-          })
-        }
-
-        const {data} = await fetch('http://localhost:5000/stripe', fetchParams)
-        console.log(data)
-      } catch (error) {
-        console.log(error)
+    try {
+      const fetchParams = {
+        crossDomain: true,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          'id': id,
+          'amount': 12345
+        })
       }
+
+      const { data } = await fetch('http://localhost:5000/stripe', fetchParams)
+      console.log('success')
+      console.log(data)
+    } catch (error) {
+      console.log('fail')
+      console.log(error)
     }
+  }
+
+  const test = async (e) => {
+    e.preventDefault()
+    const response = await fetch('http://localhost:5000/test', testParams)
+    console.log(response)
   }
 
   return (
     <div className='stripe-container'>
       <form onSubmit={handleSubmit} className='stripe-form'>
         <CardElement />
-        <CustomButton className='custom-button high-emphasis-button green-button'>Pay</CustomButton>
       </form>
+      <CustomButton onClick={handleSubmit} className='custom-button high-emphasis-button green-button'>Pay</CustomButton>
+      <CustomButton onClick={test} className='custom-button high-emphasis-button green-button'>Test</CustomButton>
     </div>
   )
 }
