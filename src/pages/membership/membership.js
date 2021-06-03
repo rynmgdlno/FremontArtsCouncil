b65d01226f804025b5ab1cb069d91209c4a1357d
rynmgdlno <53541580+rynmgdlno@users.noreply.github.com>
2021-06-03 13:09:15 -0700

    membership page cards and living lightly toggle

--- src/pages/membership/membership.js
+++ src/pages/membership/membership.js
@@ -218,7 +218,7 @@ const Membership = () => {
               <StripeForm
                 formData={formData}
                 isDonation={false}
-                product={living ? `living lightly membership` : `membership`}
+                product={living ? `living lightly ${level} membership` : `${level} membership`}
                 clicked={clicked}
                 setClicked={setClicked}
                 fixForm={fixForm}
